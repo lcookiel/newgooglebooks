fd4cc966d0969285341caa0175a9077c33d75f4c
lazarus <eleazar.levchenko@gmail.com>
2020-06-24 09:52:47 +0300

    initial commit

---
+++ js/background.js
@@ -0,0 +1,31 @@
+
+chrome.tabs.onCreated.addListener(function(){
+  console.log("tab created")
+});
+
+
+chrome.tabs.onUpdated.addListener(function(){
+  console.log("tab updated")
+  chrome.tabs.query(
+ {
+  lastFocusedWindow: true,
+  active: true
+ },
+ function (tabs)
+ {
+  var currenturl = tabs[0].url
+  var urlstatus = tabs[0].status
+  // var title = tabs[0].title
+  var domain = currenturl.split("/")[2].split(".").slice(0,2) // [books, google] OR [www, google]
+  var check = currenturl.split("/")[3] // [books, google] OR [www, google]
+  if (domain[0] === "books"
+  && domain[1] === "google"
+  && check.split("?")[0].substring(0,5) === "books"
+  && check.split("?")[1].split("&")[0].split("=")[0] === "id") {
+    console.log("hello");
+    var id = check.split("?")[1].split("&")[0].split("=")[1]
+    var newurl = "https://www.google.com/books/edition/0/" + id
+    chrome.tabs.update(null, {url:newurl});
+  };
+ });
+});
