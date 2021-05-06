4de6bdf5844ea6f9decf030619891cb2697d95c4
lcookiel <eleazar.levchenko@hotmail.com>
2021-05-06 20:46:30 +0300

    fix bug, optimize

--- js/background.js
+++ js/background.js
@@ -1,23 +1,19 @@
-chrome.tabs.onUpdated.addListener(function(){
-  chrome.tabs.query(
- {
-  lastFocusedWindow: true,
-  active: true
- },
- function (tabs)
- {
-  var currenturl = tabs[0].url
-  var urlstatus = tabs[0].status
-  var domain = currenturl.split("/")[2].split(".").slice(0,2) // [books, google] OR [www, google]
-  var check = currenturl.split("/")[3] // [books, google] OR [www, google]
-  if (domain[0] === "books"
-  && domain[1] === "google"
-  && check.split("?")[0].substring(0,5) === "books"
-  && check.split("?")[1].split("&")[0].split("=")[0] === "id") {
-    console.log("hello");
-    var id = check.split("?")[1].split("&")[0].split("=")[1]
-    var newurl = "https://www.google.com/books/edition/0/" + id
-    chrome.tabs.update(null, {url:newurl});
-  };
- });
+chrome.tabs.onUpdated.addListener(function() {
+  chrome.tabs.query({
+      lastFocusedWindow: true,
+      active: true
+    },
+    function(tabs) {
+      var currenturl = tabs[0].url;
+      var domain = currenturl.split('/')[2].substring(0, 16);
+      var url = new URL(currenturl);
+      var args = new URLSearchParams(url.search);
+      var id = args.get('id');
+      if (domain == 'books.google.com' && id != null) {
+        var newurl = "https://www.google.com/books/edition/_/" + id
+        chrome.tabs.update(null, {
+          url: newurl
+        });
+      };
+    });
 });
