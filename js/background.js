ec80d3fdb27bc14c08a7dbef27fa050beec243a6
Lazarus <eleazar.levchenko@gmail.com>
2022-09-11 13:07:39 +0300

    Fix the bug with domain recognition

--- js/background.js
+++ js/background.js
@@ -1,16 +1,16 @@
-chrome.tabs.onUpdated.addListener(function() {
+chrome.tabs.onUpdated.addListener(function () {
   chrome.tabs.query({
-      lastFocusedWindow: true,
-      active: true
-    },
-    function(tabs) {
+    lastFocusedWindow: true,
+    active: true
+  },
+    function (tabs) {
       var currenturl = tabs[0].url;
-      var domain = currenturl.split('/')[2].substring(0, 16);
+      var googlebooksmatch = RegExp("(?:https?:\/\/)?(?:www.)?books.google.[a-zA-Z]*").test(currenturl);
       var url = new URL(currenturl);
       var args = new URLSearchParams(url.search);
       var id = args.get('id');
-      if (domain == 'books.google.com' && id != null) {
-        var newurl = "https://www.google.com/books/edition/_/" + id
+      if (googlebooksmatch && id != null) {
+        var newurl = "https://www.google.com/books/edition/_/" + id;
         chrome.tabs.update(null, {
           url: newurl
         });
