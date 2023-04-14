d041234f9a221ace205ab4ee231a83ef25c55a2d
Lazarus <eleazar.levchenko@gmail.com>
2023-04-15 01:34:59 +0300

    Improve URL handling to prevent constructor errors

--- js/background.js
+++ js/background.js
@@ -6,14 +6,17 @@ chrome.tabs.onUpdated.addListener(function () {
     function (tabs) {
       var currenturl = tabs[0].url;
       var googlebooksmatch = RegExp("(?:https?:\/\/)?(?:www.)?books.google.[a-zA-Z]*").test(currenturl);
-      var url = new URL(currenturl);
-      var args = new URLSearchParams(url.search);
-      var id = args.get('id');
-      if (googlebooksmatch && id != null) {
-        var newurl = "https://www.google.com/books/edition/_/" + id;
-        chrome.tabs.update(null, {
-          url: newurl
-        });
-      };
+
+      if (googlebooksmatch) {
+        var url = new URL(currenturl);
+        var args = new URLSearchParams(url.search);
+        var id = args.get('id');
+        if (id != null) {
+          var newurl = "https://www.google.com/books/edition/_/" + id;
+          chrome.tabs.update(null, {
+            url: newurl
+          });
+        };
+      }
     });
 });
